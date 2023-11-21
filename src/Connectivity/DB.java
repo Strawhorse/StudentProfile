4249d244f844fefaad2697152c08dc286825eeed
Sean B <strawhorse@gmail.com>
2023-11-21 20:13:04 +0000

    Initial commit

---
+++ src/Connectivity/DB.java
@@ -0,0 +1,24 @@
+package Connectivity;
+import java.sql.*;
+
+public class DB {
+
+//    create connection with database
+
+    public static Connection getCon() {
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc://mysql://localhost/student", "root", "root");
+            System.out.println("Connection successful");
+            return con;
+
+        } catch (SQLException e) {
+            System.out.println(e);
+         } catch (ClassNotFoundException e) {
+            System.out.println(e);
+        }
+        return null;
+    }
+
+
+}
